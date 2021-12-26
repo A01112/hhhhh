eeab3184ce15740843862ca4a190a0eba8b0a00c
A01112 <96502289+A01112@users.noreply.github.com>
2021-12-26 19:04:23 +0200

    Add files via upload

---
+++ Healthapp/BMI.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class BMI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BMI));
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.tbWeight = new System.Windows.Forms.TextBox();
+            this.tbAge = new System.Windows.Forms.TextBox();
+            this.tbHeight = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bmicalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.BackColor = System.Drawing.Color.Transparent;
+            this.rbFemale.Location = new System.Drawing.Point(486, 87);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(71, 21);
+            this.rbFemale.TabIndex = 26;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = false;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.BackColor = System.Drawing.Color.Transparent;
+            this.rbMale.Location = new System.Drawing.Point(411, 87);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(55, 21);
+            this.rbMale.TabIndex = 25;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = false;
+            // 
+            // tbWeight
+            // 
+            this.tbWeight.Location = new System.Drawing.Point(452, 263);
+            this.tbWeight.Name = "tbWeight";
+            this.tbWeight.Size = new System.Drawing.Size(50, 24);
+            this.tbWeight.TabIndex = 23;
+            this.tbWeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbAge
+            // 
+            this.tbAge.Location = new System.Drawing.Point(452, 181);
+            this.tbAge.Name = "tbAge";
+            this.tbAge.Size = new System.Drawing.Size(50, 24);
+            this.tbAge.TabIndex = 22;
+            this.tbAge.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Location = new System.Drawing.Point(452, 224);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Size = new System.Drawing.Size(50, 24);
+            this.tbHeight.TabIndex = 21;
+            this.tbHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(433, 137);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 24);
+            this.tbName.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(243, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(243, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Weight (kg)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(243, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Height (cm)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(243, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Age";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(243, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Name";
+            // 
+            // bmicalculate
+            // 
+            this.bmicalculate.AllowDrop = true;
+            this.bmicalculate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bmicalculate.AutoSize = true;
+            this.bmicalculate.BackColor = System.Drawing.Color.Transparent;
+            this.bmicalculate.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bmicalculate.BackgroundImage")));
+            this.bmicalculate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bmicalculate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bmicalculate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.bmicalculate.Location = new System.Drawing.Point(300, 312);
+            this.bmicalculate.Name = "bmicalculate";
+            this.bmicalculate.Size = new System.Drawing.Size(166, 52);
+            this.bmicalculate.TabIndex = 24;
+            this.bmicalculate.Text = "                    ";
+            this.bmicalculate.UseVisualStyleBackColor = false;
+            // 
+            // BMI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.bmicalculate);
+            this.Controls.Add(this.tbWeight);
+            this.Controls.Add(this.tbAge);
+            this.Controls.Add(this.tbHeight);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BMI";
+            this.Text = "BMI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.Button bmicalculate;
+        private System.Windows.Forms.TextBox tbWeight;
+        private System.Windows.Forms.TextBox tbAge;
+        private System.Windows.Forms.TextBox tbHeight;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Healthapp/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBMR = new System.Windows.Forms.Button();
+            this.btnBMI = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBMR
+            // 
+            this.btnBMR.BackColor = System.Drawing.Color.White;
+            this.btnBMR.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBMR.BackgroundImage")));
+            this.btnBMR.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBMR.Location = new System.Drawing.Point(290, 291);
+            this.btnBMR.Name = "btnBMR";
+            this.btnBMR.Size = new System.Drawing.Size(90, 47);
+            this.btnBMR.TabIndex = 0;
+            this.btnBMR.UseVisualStyleBackColor = false;
+            this.btnBMR.Click += new System.EventHandler(this.btnBMR_Click);
+            // 
+            // btnBMI
+            // 
+            this.btnBMI.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnBMI.BackColor = System.Drawing.Color.White;
+            this.btnBMI.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBMI.BackgroundImage")));
+            this.btnBMI.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBMI.Location = new System.Drawing.Point(431, 291);
+            this.btnBMI.Name = "btnBMI";
+            this.btnBMI.Size = new System.Drawing.Size(90, 47);
+            this.btnBMI.TabIndex = 1;
+            this.btnBMI.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBMI);
+            this.Controls.Add(this.btnBMR);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Health";
+         
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBMR;
+        private System.Windows.Forms.Button btnBMI;
+    }
+}
